da8409d46180e192f48fe9cd4469867e27439c0b
Alejandro Coca <alxmcr@outlook.com>
2024-01-27 19:39:03 -0400

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './pages/HomePage';
+import { RouterProvider } from 'react-router-dom';
 import './index.scss';
+import { appRouter } from './router/routes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HomePage />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>,
 );
