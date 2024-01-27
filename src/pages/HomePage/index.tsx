ef0bbdc4650de6b91a4183c125ae73c4941ad6b0
Alejandro Coca <alxmcr@outlook.com>
2024-01-27 19:49:51 -0400

    wip

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -8,6 +8,16 @@ export default function HomePage() {
       <IconWeather />
       <h1 className="home-page__title">Weather App</h1>
       <CardFormSearchCity />
+      <p className="home-page__poweredby">
+        {'Powered by '}
+        <a
+          href="https://www.weatherapi.com/"
+          title="Free Weather API"
+          className="home-page__link"
+        >
+          WeatherAPI.com
+        </a>
+      </p>
     </main>
   );
 }
