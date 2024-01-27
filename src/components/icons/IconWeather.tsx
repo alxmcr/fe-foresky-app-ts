46f0baa06e40f897f3403ff6c0cd3a8d5176aa82
Alejandro Coca <alxmcr@outlook.com>
2024-01-27 19:11:04 -0400

    wip

--- src/components/icons/IconWeather.tsx
+++ src/components/icons/IconWeather.tsx
@@ -1,47 +1,22 @@
 export default function IconWeather() {
   return (
-    <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
-      <g
-        id="SVGRepo_tracerCarrier"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <g id="SVGRepo_iconCarrier">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={64}
+      height={64}
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <g stroke="#1C274C" strokeWidth={1.5}>
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth={1.5}
-          d="M8 22H16"
+          d="M8 22h8M5 19h14M2 16h20"
         />
+        <path d="M12 6a6 6 0 0 0-4.5 9.969h9A6 6 0 0 0 12 6Z" />
         <path
           strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.5}
-          d="M5 19H19"
-        />
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={1.5}
-          d="M2 16H22"
-        />
-        <path
-          strokeWidth={1.5}
-          d="M12 6C8.68629 6 6 8.68629 6 12C6 13.5217 6.56645 14.911 7.5 15.9687H16.5C17.4335 14.911 18 13.5217 18 12C18 8.68629 15.3137 6 12 6Z"
-        />
-        <path strokeLinecap="round" strokeWidth={1.5} d="M12 2V3" />
-        <path strokeLinecap="round" strokeWidth={1.5} d="M22 12L21 12" />
-        <path strokeLinecap="round" strokeWidth={1.5} d="M3 12L2 12" />
-        <path
-          strokeLinecap="round"
-          strokeWidth={1.5}
-          d="M19.0708 4.92969L18.678 5.32252"
-        />
-        <path
-          strokeLinecap="round"
-          strokeWidth={1.5}
-          d="M5.32178 5.32227L4.92894 4.92943"
+          d="M12 2v1M22 12h-1M3 12H2M19.07 4.93l-.392.393M5.322 5.322l-.393-.393"
         />
       </g>
     </svg>
