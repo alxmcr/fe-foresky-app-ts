3fa2183b848fbfb7da706123c64741e33dad0794
Alejandro Coca <alxmcr@outlook.com>
2024-01-28 18:16:30 -0400

    wip

--- src/components/cards/CardForecastToday/index.tsx
+++ src/components/cards/CardForecastToday/index.tsx
@@ -1,33 +1,16 @@
-import React from 'react';
-import { LoadingStates } from '../../../@types/appTypes';
-import useCurrentWeather from '../../../hooks/useCurrentWeather';
-import { CityContext } from '../../../providers/CityContext';
+import { Weather } from '../../../@types/typeForecasts';
 import CardBodyForecastToday from '../CardBodyForecastToday';
 import CardFooterForecastToday from '../CardFooterForecastToday';
 import CardHeaderForecastToday from '../CardHeaderForecastToday';
 import './CardForecastToday.scss';
 
-export default function CardForecastToday() {
-  const { nameCity } = React.useContext(CityContext);
-  const { currentWeather, loading, error } = useCurrentWeather(nameCity);
-  console.log({ currentWeather, loading, error });
+type CardForecastTodayProps = {
+  currentWeather: Weather;
+};
 
-  if (loading === LoadingStates.PENDING) {
-    return (
-      <article className="card-forecast-today">
-        <p className="card-forecast-today__message">Loading...</p>
-      </article>
-    );
-  }
-
-  if (loading === LoadingStates.ERROR && error !== null) {
-    return (
-      <article className="card-forecast-today">
-        <p className="card-forecast-today__message">{error.message}</p>
-      </article>
-    );
-  }
-
+export default function CardForecastToday({
+  currentWeather,
+}: CardForecastTodayProps) {
   return (
     <article className="card-forecast-today">
       <CardHeaderForecastToday currentWeather={currentWeather || null} />
--- src/pages/ForecastPage/index.tsx
+++ src/pages/ForecastPage/index.tsx
@@ -4,26 +4,58 @@ import SectionGroupForecastByHour from '../../components/sections/SectionGroupFo
 import SectionGroupForecastByWeek from '../../components/sections/SectionGroupForecastByWeek';
 import './ForecastPage.scss';
 import { HomeRoute } from '../../router/constants.routes';
+import React from 'react';
+import { CityContext } from '../../providers/CityContext';
+import useCurrentWeather from '../../hooks/useCurrentWeather';
+import { LoadingStates } from '../../@types/appTypes';
 
 export default function ForecastPage() {
-  return (
-    <main className="forecast-page">
-      <NavLink to={HomeRoute.path} className="forecast-page__link">
-        Return to home
-      </NavLink>
-      <CardForecastToday />
-      <SectionGroupForecastByHour />
-      <SectionGroupForecastByWeek />
-      <p className="forecast-page__poweredby">
-        {'Powered by '}
-        <a
-          href="https://www.weatherapi.com/"
-          title="Free Weather API"
-          className="forecast-page__link"
-        >
-          WeatherAPI.com
-        </a>
-      </p>
-    </main>
-  );
+  const { nameCity } = React.useContext(CityContext);
+  const { currentWeather, loading, error } = useCurrentWeather(nameCity);
+  console.log({ currentWeather, loading, error });
+
+  if (loading === LoadingStates.PENDING) {
+    return (
+      <article className="card-forecast-today">
+        <p className="card-forecast-today__message">Loading...</p>
+      </article>
+    );
+  }
+
+  if (loading === LoadingStates.ERROR && error !== null) {
+    return (
+      <article className="card-forecast-today">
+        <p className="card-forecast-today__message">{error.message}</p>
+
+        <NavLink to={HomeRoute.path} className="forecast-page__link">
+          Return to home
+        </NavLink>
+      </article>
+    );
+  }
+
+  if (currentWeather !== null) {
+    return (
+      <main className="forecast-page">
+        <NavLink to={HomeRoute.path} className="forecast-page__link">
+          Return to home
+        </NavLink>
+        <CardForecastToday currentWeather={currentWeather} />
+        <SectionGroupForecastByHour />
+        <SectionGroupForecastByWeek />
+        <p className="forecast-page__poweredby">
+          {'Powered by '}
+          <a
+            href="https://www.weatherapi.com/"
+            title="Free Weather API"
+            className="forecast-page__link"
+          >
+            WeatherAPI.com
+          </a>
+        </p>
+      </main>
+    );
+  }
+
+  return null;
 }
