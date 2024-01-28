23c25f4ecd2096558b000a7f4ee636afd6f41346
Alejandro Coca <alxmcr@outlook.com>
2024-01-28 14:47:48 -0400

    wip

--- src/components/sections/SectionGroupForecastByWeek/index.tsx
+++ src/components/sections/SectionGroupForecastByWeek/index.tsx
@@ -3,14 +3,14 @@ import { LoadingStates } from '../../../@types/appTypes';
 import useForecastsThisWeek from '../../../hooks/useForecastsThisWeek';
 import { CityContext } from '../../../providers/CityContext';
 
-export default function SectionGroupForecastByNextDays() {
+export default function SectionGroupForecastByWeek() {
   const { nameCity } = React.useContext(CityContext);
   const { forecasts, loading, error } = useForecastsThisWeek(nameCity);
 
   if (loading === LoadingStates.PENDING) {
     return (
-      <section className="section-group-forecasts-by-nextdays">
-        <p className="section-group-forecasts-by-nextdays__message">
+      <section className="section-group-forecasts-this-week">
+        <p className="section-group-forecasts-this-week__message">
           Loading forecasts...
         </p>
       </section>
@@ -19,8 +19,8 @@ export default function SectionGroupForecastByNextDays() {
 
   if (loading === LoadingStates.ERROR) {
     return (
-      <section className="section-group-forecasts-by-nextdays">
-        <p className="section-group-forecasts-by-nextdays__message">
+      <section className="section-group-forecasts-this-week">
+        <p className="section-group-forecasts-this-week__message">
           {error?.message}
         </p>
       </section>
@@ -28,8 +28,8 @@ export default function SectionGroupForecastByNextDays() {
   }
 
   return (
-    <section className="section-group-forecasts-by-nextdays">
-      <h2 className="section-group-forecasts-by-nextdays__title">This week</h2>
+    <section className="section-group-forecasts-this-week">
+      <h2 className="section-group-forecasts-this-week__title">This week</h2>
       {JSON.stringify(forecasts, null, 2)}
     </section>
   );
--- src/pages/ForecastPage/index.tsx
+++ src/pages/ForecastPage/index.tsx
@@ -1,6 +1,6 @@
 import CardForecastToday from '../../components/cards/CardForecastToday';
 import SectionGroupForecastByHour from '../../components/sections/SectionGroupForecastByHour';
-import SectionGroupForecastByNextDays from '../../components/sections/SectionGroupForecastByWeek';
+import SectionGroupForecastByWeek from '../../components/sections/SectionGroupForecastByWeek';
 import './ForecastPage.scss';
 
 export default function ForecastPage() {
@@ -8,7 +8,7 @@ export default function ForecastPage() {
     <main className="forecast-page">
       <CardForecastToday />
       <SectionGroupForecastByHour />
-      <SectionGroupForecastByNextDays />
+      <SectionGroupForecastByWeek />
     </main>
   );
 }
