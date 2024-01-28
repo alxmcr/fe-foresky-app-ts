a20ca7d9b917fe2a2ee3e3dfdfed859a2339cc11
Alejandro Coca <alxmcr@outlook.com>
2024-01-28 00:02:20 -0400

    wip

---
+++ src/components/cards/CardFooterForecastToday/index.tsx
@@ -0,0 +1,45 @@
+import { CurrentForecast } from '../../../@types/typeForecasts';
+import IconCloud from '../../icons/IconCloud';
+import IconDropWater from '../../icons/IconDropWater';
+import IconWind from '../../icons/IconWind';
+
+type CardFooterForecastTodayProps = {
+  currentForecast: CurrentForecast | null;
+};
+
+export default function CardFooterForecastToday({
+  currentForecast,
+}: CardFooterForecastTodayProps) {
+  if (currentForecast === null) {
+    return null;
+  }
+
+  return (
+    <footer className="card-footer-forecast-today">
+      <div className="card-footer-forecast-today__nature-info">
+        <i className="card-footer-forecast-today__icon">
+          <IconWind />
+        </i>
+        <p className="card-footer-forecast-today__nature-event">
+          {currentForecast.current.wind_kph} Km/h
+        </p>
+      </div>
+      <div className="card-footer-forecast-today__nature-info">
+        <i className="card-footer-forecast-today__icon">
+          <IconDropWater />
+        </i>
+        <p className="card-footer-forecast-today__nature-event">
+          {currentForecast.current.humidity}%
+        </p>
+      </div>
+      <div className="card-footer-forecast-today__nature-info">
+        <i className="card-footer-forecast-today__icon">
+          <IconCloud />
+        </i>
+        <p className="card-footer-forecast-today__nature-event">
+          {currentForecast.current.cloud}%
+        </p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/icons/IconCloud.tsx
@@ -0,0 +1,19 @@
+export default function IconCloud() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={44}
+      height={44}
+      fill="none"
+      strokeWidth={1.5}
+      color="#000"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#000"
+        strokeLinejoin="round"
+        d="M12 4c-6 0-6 4-6 6-1.667 0-5 1-5 5s3.333 5 5 5h12c1.667 0 5-1 5-5s-3.333-5-5-5c0-2 0-6-6-6Z"
+      />
+    </svg>
+  );
+}
